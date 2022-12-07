bc3ea6b8f57fd5ce9458913e50e0520b46c09bec
brian.maina@student.moringaschool.com <brianmaina2>
2022-12-07 11:28:30 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      handleToDo(e.target.new_task_description.value)
+      form.reset()
+  })
 });
+
+function handleToDo(toDo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "x"
+  p.textContent = `${toDo}`
+  p.appendChild(btn)
+  console.log(p);
+  document.querySelector("#list").appendChild(p);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
